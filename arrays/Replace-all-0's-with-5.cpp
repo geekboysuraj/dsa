f644b6b184d89e6e5ec277072ae37944c9ba06b5
Suraj <29368267+geekboysuraj@users.noreply.github.com>
2021-03-25 17:59:13 +0530

    Create Replace-all-0's-with-5.cpp

---
+++ arrays/Replace-all-0's-with-5.cpp
@@ -0,0 +1,35 @@
+//https://practice.geeksforgeeks.org/problems/replace-all-0s-with-5/1/
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int convertFive(int n);
+
+// Driver program to test above function
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << convertFive(n) << endl;
+    }
+}// } Driver Code Ends
+
+
+/*you are required to complete this method*/
+int convertFive(int n) {
+    int result;
+    string s = to_string(n);
+    for(int i=0;i<s.size();++i){
+        if(s[i]=='0'){
+            s[i]='5';
+        }
+        result=stoi(s);
+        
+        
+    }
+    return result;
+}
